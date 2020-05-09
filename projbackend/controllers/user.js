6e5dfeadc9f641b89e9a596e583618a831f858a9
Ashish Purohit <ashishpurohit3007@gmail.com>
2020-05-10 03:27:54 +0530

    Updating User Controller

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -2,20 +2,19 @@ const User = require("../models/user");
 const Order = require("../models/order");
 
 //Controller Method to get User by passing id as a param//
-exports.getUserById = (req,res,next,id) =>{
-  User.findById(id).exec((err, user) =>{
-    if(err || !user)
-    {
-     return res.status(400).json({
-       error : "No User was found in the Database"
-     })
+exports.getUserById = (req, res, next, id) => {
+  User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "No User was found in the Database",
+      });
     }
     req.profile = user;
     next();
-  })
-}
+  });
+};
 
-exports.getUser = (req, res) =>{
+exports.getUser = (req, res) => {
   //TODO : get back here for password
   req.profile.salt = undefined;
   req.profile.encry_password = undefined;
@@ -24,37 +23,68 @@ exports.getUser = (req, res) =>{
   return res.json(req.profile);
 };
 
-exports.updateUser = (req,res) => {
+exports.updateUser = (req, res) => {
   User.findByIdAndUpdate(
-    {_id : req.profile._id},
-    {$set : req.body},
-    {new : true, useFindAndModify : false},
-    (err, user) =>{
-      if(err || !user)
-      return res.status(400).json({
-        error :  "You're not Authorized to update this User"
-      })
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err || !user)
+        return res.status(400).json({
+          error: "You're not Authorized to update this User",
+        });
       console.log(user, " saknsabkbc");
       user.salt = undefined;
       user.encry_password = undefined;
       user.createdAt = undefined;
       user.updatedAt = undefined;
-      return res.json(user)
+      return res.json(user);
     }
-  )
-}
+  );
+};
 
-exports.userPurchaseList = (req,res) =>
-{
-  Order.find({user : req.profile._id})
-  .populate("user" , "_id name")
-  .exec((err, order) => {
-    if(err)
-    {
-      return res.status(400).json({
-        error : "No order in this account"
-      })
+exports.userPurchaseList = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .populate("user", "_id name")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "No order in this account",
+        });
+      }
+      return res.json(order);
+    });
+};
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+  console.log(req,"Req from pushOrder in Purchase List");
+  console.log(res , "Res from push order in purchase list");
+  let purchases = [];
+  req.body.order.products.forEach((product) => {
+    console.log(product, "Product from Callback");
+    products.push({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      amount: req.body.order.amount,
+      transaction_id: req.body.order.transaction_id,
+    });
+  });
+  console.log(purchases, "Purchases");
+  //Store in DB
+
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $push: { purchases: purchases } },
+    { new: true },
+    (err, purchaes) => {
+      if (err)
+        return res.status(400).json({
+          error: "Unable to save purchase list!",
+        });
     }
-    return res.json(order)
-  })
-}
+  );
+  next();
+};
